533b595dbc0baef0d07cfde38c597195c3f77c27
Shihab-Mehboob1 <shihab.mehboob1@ibm.com>
2018-08-03 15:42:31 +0100

    Add dismiss button to info view

--- ShareIt/InfoViewController.swift
+++ ShareIt/InfoViewController.swift
@@ -2,7 +2,7 @@
 //  InfoViewController.swift
 //  ShareIt
 //
-//  Created by Shibab Mehboob on 03/08/2018.
+//  Created by Andrew Lees, David Dunn, Kye Maloy, and Shihab Mehboob on 03/08/2018.
 //  Copyright © 2018 IBM. All rights reserved.
 //
 
@@ -11,10 +11,23 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
+    var dismissButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's background colour
         self.view.backgroundColor = Colours.white
+        
+        // Add dismiss button
+        self.dismissButton.frame = CGRect(x: 20, y: 20, width: 40, height: 40)
+        self.dismissButton.backgroundColor = Colours.offWhite
+        self.dismissButton.layer.cornerRadius = CGFloat(20)
+        self.dismissButton.addTarget(self, action: #selector(self.dismissView), for: .touchUpInside)
+        self.view.addSubview(self.dismissButton)
+    }
+    
+    @objc func dismissView(button: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
