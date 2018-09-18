a5fa01dd6d3b09e33cc8844b21160c57a65ec4de
Shihab-Mehboob1 <shihab.mehboob1@ibm.com>
2018-09-18 11:38:41 +0100

    Changed colours

--- ShareIt/Colours.swift
+++ ShareIt/Colours.swift
@@ -15,10 +15,10 @@ struct Colours {
     static var white = UIColor.white
     static var clear = UIColor.clear
     static var red = UIColor(red: 250/255, green: 10/255, blue: 10/255, alpha: 1)
-    static var purpleLight = UIColor(red: 169/255, green: 91/255, blue: 206/255, alpha: 1)
-    static var purple = UIColor(red: 137/255, green: 76/255, blue: 183/255, alpha: 1)
+    static var purpleLight = UIColor(red: 48/255, green: 160/255, blue: 200/255, alpha: 1)
+    static var purple = UIColor(red: 78/255, green: 185/255, blue: 225/255, alpha: 1)
     static var offWhite = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
     
     // TO DO: Agree on main app tint colour, for now make it purple
-    static var appTintColour = UIColor(red: 137/255, green: 76/255, blue: 183/255, alpha: 1)
+    static var appTintColour = UIColor(red: 78/255, green: 185/255, blue: 225/255, alpha: 1)
 }
